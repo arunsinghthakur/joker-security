64e7e291d24e7f69a632a5b7a1e5bcb1c710e04b
arunsingh <arunsingh@Arun373501.Nagarro.local>
2018-06-28 14:53:31 +0530

    kk

--- joker-security-resource-server/src/main/java/com/joker/security/config/JokerResourceServer.java
+++ joker-security-resource-server/src/main/java/com/joker/security/config/JokerResourceServer.java
@@ -3,8 +3,6 @@ package com.joker.security.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class JokerResourceServer extends SpringBootServletInitializer{
